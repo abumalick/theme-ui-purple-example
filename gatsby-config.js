d5287bc0f5e0d681750ef7e943bb400653e9c057
abumalick <abdullah.hilson@gmail.com>
2020-04-19 18:02:50 +0000

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+  plugins: ["gatsby-plugin-theme-ui"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function () {
+  return (
+    <h1
+      style={{
+        color: "#fff",
+      }}
+    >
+      I am a purple page
+    </h1>
+  );
+}
